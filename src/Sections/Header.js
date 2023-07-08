f127465982dbe353da50f187bde4681dfc9e488c
Rachel Spencer <rachelspencer688@gmail.com>
2023-07-08 14:22:04 -0400

    styling for intro iphone media query complete

--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -1,7 +1,9 @@
+import headerlogo from "../images/headerlogo.jpg"
+
 function Header() {
     return (
-        <div>
-            DevVlt
+        <div style={{display:"flex", justifyContent:"center", margin:"25px"}}>
+            <img src={headerlogo} style={{width:"130px"}}alt="Dev Vault logo"/>
         </div>
     )
 };
--- src/Sections/Intro.js
+++ src/Sections/Intro.js
@@ -9,7 +9,7 @@ function Intro (){
         <div className="intro">
             <Panel className="intro-panel">
                 <h1>Welcome to the Dev Vault!</h1>
-                <p>This vault stores all the valuable content you come across. Time to close the 50 tabs you have open!</p>
+                <p>This vault stores all the valuable web dev content you come across. Close the 50 tabs you have open and save it to your vault!</p>
                 <img className="logo" src={logo} alt="logo"/>
             </Panel>
             <Panel className="intro-panel">
